89cea08064af4521b0659d0b7c01dc506d3d748a
yegenie2 <111625018+yegenie2@users.noreply.github.com>
2023-09-21 21:33:40 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+
+chat_model = ChatOpenAI()
+
+st.header('소설 봇', divider='rainbow')
+st.title('	:heartpulse: :rainbow[AI 동화] :heartpulse:')
+
+content = st.text_input('동화의 주제를 제시해주세요.')
+
+if st.button('동화 작성 요청하기'):
+    with st.spinner('동화 작성 중...'):
+        result = chat_model.predict(content + "에 대한 동화 작성해줘 글자수는 1000자 미만으로 ")
+        st.write(result)
+
+
+
+
+
+
+
+
+
+
+
